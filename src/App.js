5c813c83f76c2ac04764a7b602ecf2a7dcc7b74b
renyellen machado <101356078+renyellen@users.noreply.github.com>
2022-08-04 22:30:33 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  state = {
+    pessoa: [
+      { nome: "Renyellen", idade:25 , cidade:"São Paulo"},
+  { nome: "Danyellen", idade:20 , cidade:"São Paulo"},
+  { nome: "Carlos Eduardo", idade:21 , cidade:"São Paulo"},
+      { nome: "Rafael", idade:40  , cidade:"São Paulo"},
+  { nome: "Rafaela", idade:25 , cidade:"São Paulo"}
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.pessoa.map((item) => (
+          <p>
+            Minha família é composta por :
+            <ul>
+            <li>{item.nome}, {item.idade}, {item.cidade}
+          
+          
+        ))}
+      </div>
+    );
+  }
+}
